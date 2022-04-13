04bd9831dfa6820d38914ff2b878475ff2c3a603
Eduardo Santana Rossetto <edusrossetto@gmail.com>
2022-04-13 15:54:02 -0300

    Adicionando classes mockads para ajudar a reproduzir as classes na pasta security (bd e token)

---
+++ src/br/com/lightflow/securitymock/TokenMock.java
@@ -0,0 +1,9 @@
+package br.com.lightflow.securitymock;
+
+public class TokenMock {
+    private final static String token="your discord bot token here";
+
+    public static String getToken(){
+        return token;
+    }
+}
