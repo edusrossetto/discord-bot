b0af8eb7a26b440ecb247e98e91359dcf4426797
Eduardo Santana Rossetto <edusrossetto@gmail.com>
2022-04-18 11:14:46 -0300

    Suprimindo aviso de variável não usada no SqliteMock

--- src/br/com/lightflow/securitymock/SqliteMock.java
+++ src/br/com/lightflow/securitymock/SqliteMock.java
@@ -1,7 +1,8 @@
 package br.com.lightflow.securitymock;
 
 public class SqliteMock {
-    private final String mensagem = "Use o tutorial https://www.sqlitetutorial.net/sqlite-java/ para reproduzir o bd do bot, alguns links não estão funcionando então você tem que copiá-los e jogá-los no https://archive.org/web/ para ver o site antes quando funcionava, https://www.youtube.com/watch?v=wXEZZ2JT3-k esse tutorial te ensina a instalar o SQLite, eu uso o db browser para gerenciar o bd";
-
     
+    @SuppressWarnings("unused")
+    private final String mensagem = "Use o tutorial https://www.sqlitetutorial.net/sqlite-java/ para reproduzir o bd do bot, alguns links não estão funcionando então você tem que copiá-los e jogá-los no https://archive.org/web/ para ver o site antes quando funcionava, https://www.youtube.com/watch?v=wXEZZ2JT3-k esse tutorial te ensina a instalar o SQLite, eu uso o db browser para gerenciar o bd";
+     
 }
